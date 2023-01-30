73df21149bcbcfc471d46e29ece724c72d03cf5e
ColinGreenleaf <colingleaf@gmail.com>
2023-01-30 14:32:45 -0500

    added sensor to front leg, and made graph of sensor data

--- analyze.py
+++ analyze.py
@@ -2,7 +2,10 @@ import numpy as numpy
 import matplotlib.pyplot as plt
 
 backLegSensorValues = numpy.load("data/backLegSensorValues.npy")
+frontLegSensorValues = numpy.load("data/frontLegSensorValues.npy")
 print(backLegSensorValues)
 
-plt.plot(backLegSensorValues)
+plt.plot(backLegSensorValues, label="back leg", linewidth=2)
+plt.plot(frontLegSensorValues, label="front leg",)
+plt.legend()
 plt.show()
--- simulate.py
+++ simulate.py
@@ -13,12 +13,16 @@ robotId = p.loadURDF("body.urdf")
 p.loadSDF("world.sdf")
 
 pyrosim.Prepare_To_Simulate(robotId)
-backLegSensorValues = numpy.zeros(100)
-for i in range(100):
+backLegSensorValues = numpy.zeros(500)
+frontLegSensorValues = numpy.zeros(500)
+
+for i in range(500):
     p.stepSimulation()
     backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
+    frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
     time.sleep(1./1000.)
     print(i)
 numpy.save("data/backLegSensorValues.npy", backLegSensorValues)
+numpy.save("data/frontLegSensorValues.npy", frontLegSensorValues)
 p.disconnect()
 
