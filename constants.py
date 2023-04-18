ad041b3745e6083ad8a56dc76d7259778ef51d6f
ColinGreenleaf <colingleaf@gmail.com>
2023-04-18 08:24:57 -0400

    tweaks

--- constants.py
+++ constants.py
@@ -1,7 +1,7 @@
 from math import pi
 
 loopAmt = 1000
-motorForce = 50
+motorForce = 150
 
 amplitude = pi/4
 frequency = 10
@@ -9,18 +9,18 @@ phaseOffset = 0
 
 modToFit = pi/(loopAmt/2)
 
-populationSize = 3
-numberOfGenerations = 3
+populationSize = 10
+numberOfGenerations = 10
 
 cubeLength = 1
 cubeWidth = 1
 cubeHeight = 1
 
 
-motorJointRange = 0.3
+motorJointRange = 0.35
 
 sensorNeurons = 17
 motorNeurons = 16
-hiddenNeurons = 10
+hiddenNeurons = 5
 
-sleepTime = 1./80.
+sleepTime = 1./70.
